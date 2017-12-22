5eaafb2c853d8eed8c7f9bcb18902a55783794fb
SEA\barora <barora@egencia.com>
2017-12-22 11:54:42 -0800

    Overriding default hashCode, equals, and toString for author and book

--- spring5webApp/src/main/java/bhavya/nerdNet/spring5webApp/model/Author.java
+++ spring5webApp/src/main/java/bhavya/nerdNet/spring5webApp/model/Author.java
@@ -1,12 +1,15 @@
-package bhavya.nerdNet.spring5webApp.model;
 
+package bhavya.nerdNet.spring5webApp.model;
+/*
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+*/
 /**
  * Created by barora on 12/20/2017.
- */
+ *//*
+
 
 
 @Entity
@@ -17,11 +20,13 @@ public class Author {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
-	@ManyToMany
-	private Set<Book> books = new HashSet<>();
-	
 	private String firstName;
 	private String lastName;
+	
+	@ManyToMany(mappedBy = "authors")
+	private Set<Book> books = new HashSet<>();
+	
+
 	
 	public Long getId() {
 		return id;
@@ -70,3 +75,101 @@ public class Author {
 		this.books = books;
 	}
 }
+*/
+
+
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by jt on 5/16/17.
+ */
+@Entity
+public class Author {
+	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Override
+	public String toString() {
+		return "Author{" +
+				"id=" + id +
+				", firstName='" + firstName + '\'' +
+				", lastName='" + lastName + '\'' +
+				", books=" + books +
+				'}';
+	}
+	
+	private String firstName;
+	private String lastName;
+	
+	@ManyToMany(mappedBy = "authors")
+	private Set<Book> books  = new HashSet<>();
+	
+	public Author() {
+	}
+	
+	public Author(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public Author(String firstName, String lastName, Set<Book> books) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.books = books;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public Set<Book> getBooks() {
+		return books;
+	}
+	
+	public void setBooks(Set<Book> books) {
+		this.books = books;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		
+		Author author = (Author) o;
+		
+		return id != null ? id.equals(author.id) : author.id == null;
+	}
+	
+	@Override
+	public int hashCode() {
+		return id != null ? id.hashCode() : 0;
+	}
+	
+}
+
